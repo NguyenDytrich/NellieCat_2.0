aefcabadac0c6e9803a27d2820155d8f6a3cc043
Dytrich NguyeN <nguyen.dytrich@gmail.com>
2021-03-05 15:46:56 -0800

    refactor

--- server/src/bot/commands/index.ts
+++ server/src/bot/commands/index.ts
@@ -3,7 +3,6 @@ import Rules from './rules';
 
 const prefix = '$';
 const commands = [];
-const internalCommands = [];
 
 const ping = {
   name: 'ping',
@@ -43,7 +42,6 @@ export default {
     client.internalCommands = new Discord.Collection();
 
     commands.forEach((c) => client.commands.set(c.name, c));
-    internalCommands.forEach((c) => client.command.set(c.name, c));
 
     const commandHandler = (message) => {
       // return if message doesn't start with the prefix, or the author is a bot
